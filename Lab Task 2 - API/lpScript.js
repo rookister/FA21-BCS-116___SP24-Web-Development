24de5f14b2b27b9e77507e0069945e00a29c746a
Naeem Ahmad <121370953+rookister@users.noreply.github.com>
2024-03-21 21:28:58 +0500

    Contact Us - Page + Validation Done
    ...

---
+++ Lab Task 2 - API/lpScript.js
@@ -0,0 +1,25 @@
+$(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 2000,
+    loop: true,
+    items: 3,
+    lazyload: true,
+    margin: 5,
+    padding: 5,
+    stagePadding: 5,
+    responsive: {
+        450 : {
+            items: 1,
+        },
+        850 : {
+            items: 2,
+        },
+        1000 : {
+            items: 3,
+        },
+        1400 : {
+            items: 4,
+        }
+    }
+});
